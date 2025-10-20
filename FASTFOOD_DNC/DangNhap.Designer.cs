f3eb5d8dc4597f5dbb5e5f96be958b34c5516e6f
Huy <l3lkeera@gmail.com>
2025-10-20 21:51:32 +0700

    dn dang do

--- FASTFOOD_DNC/DangNhap.Designer.cs
+++ FASTFOOD_DNC/DangNhap.Designer.cs
@@ -41,19 +41,19 @@
             this.label2 = new System.Windows.Forms.Label();
             this.txtTK = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtMK = new System.Windows.Forms.TextBox();
             this.btnDN = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.lnkDK = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(99, 9);
+            this.label1.Location = new System.Drawing.Point(143, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(214, 38);
+            this.label1.Size = new System.Drawing.Size(259, 46);
             this.label1.TabIndex = 0;
             this.label1.Text = "ĐĂNG NHẬP";
             // 
@@ -107,87 +107,94 @@
             // 
             // label2
             // 
+            this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(0, 105);
+            this.label2.Location = new System.Drawing.Point(0, 131);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(97, 23);
+            this.label2.Size = new System.Drawing.Size(113, 25);
             this.label2.TabIndex = 2;
             this.label2.Text = "Tài khoản:";
             // 
             // txtTK
             // 
-            this.txtTK.Location = new System.Drawing.Point(87, 94);
+            this.txtTK.Location = new System.Drawing.Point(119, 114);
+            this.txtTK.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtTK.Multiline = true;
             this.txtTK.Name = "txtTK";
-            this.txtTK.Size = new System.Drawing.Size(306, 34);
+            this.txtTK.Size = new System.Drawing.Size(344, 42);
             this.txtTK.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(0, 180);
+            this.label3.Location = new System.Drawing.Point(0, 225);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(97, 23);
+            this.label3.Size = new System.Drawing.Size(109, 29);
             this.label3.TabIndex = 4;
             this.label3.Text = "Mật khẩu:";
             // 
-            // textBox1
+            // txtMK
             // 
-            this.textBox1.Location = new System.Drawing.Point(87, 169);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.PasswordChar = '*';
-            this.textBox1.Size = new System.Drawing.Size(306, 34);
-            this.textBox1.TabIndex = 5;
+            this.txtMK.Location = new System.Drawing.Point(119, 212);
+            this.txtMK.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtMK.Multiline = true;
+            this.txtMK.Name = "txtMK";
+            this.txtMK.PasswordChar = '*';
+            this.txtMK.Size = new System.Drawing.Size(344, 42);
+            this.txtMK.TabIndex = 5;
             // 
             // btnDN
             // 
             this.btnDN.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDN.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnDN.Location = new System.Drawing.Point(120, 234);
+            this.btnDN.Location = new System.Drawing.Point(176, 296);
+            this.btnDN.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnDN.Name = "btnDN";
-            this.btnDN.Size = new System.Drawing.Size(183, 53);
+            this.btnDN.Size = new System.Drawing.Size(206, 66);
             this.btnDN.TabIndex = 6;
             this.btnDN.Text = "Đăng nhập";
             this.btnDN.UseVisualStyleBackColor = true;
+            this.btnDN.Click += new System.EventHandler(this.btnDN_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(13, 324);
+            this.label4.Location = new System.Drawing.Point(15, 405);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(184, 20);
+            this.label4.Size = new System.Drawing.Size(238, 25);
             this.label4.TabIndex = 7;
             this.label4.Text = "Bạn chưa có tài khoản?\r\n";
             // 
-            // linkLabel1
+            // lnkDK
             // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkLabel1.LinkColor = System.Drawing.SystemColors.HotTrack;
-            this.linkLabel1.Location = new System.Drawing.Point(216, 324);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(69, 20);
-            this.linkLabel1.TabIndex = 8;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "Đăng ký";
+            this.lnkDK.AutoSize = true;
+            this.lnkDK.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkDK.LinkColor = System.Drawing.SystemColors.HotTrack;
+            this.lnkDK.Location = new System.Drawing.Point(243, 405);
+            this.lnkDK.Name = "lnkDK";
+            this.lnkDK.Size = new System.Drawing.Size(91, 25);
+            this.lnkDK.TabIndex = 8;
+            this.lnkDK.TabStop = true;
+            this.lnkDK.Text = "Đăng ký";
             // 
-            // TrangChu
+            // DangNhap
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(813, 421);
-            this.Controls.Add(this.linkLabel1);
+            this.ClientSize = new System.Drawing.Size(915, 526);
+            this.Controls.Add(this.lnkDK);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.btnDN);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtMK);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.txtTK);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Name = "TrangChu";
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "DangNhap";
             this.Text = "Đăng Nhập";
+            this.Load += new System.EventHandler(this.DangNhap_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -207,10 +214,10 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtTK;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtMK;
         private System.Windows.Forms.Button btnDN;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel lnkDK;
     }
 }
 
