fa1a19af4d753a818db5b98257f0d4a9c9636606
Huy <l3lkeera@gmail.com>
2025-10-22 09:32:43 +0700

    phandangkymoi

--- FASTFOOD_DNC/DangKy.cs
+++ FASTFOOD_DNC/DangKy.cs
@@ -33,6 +33,7 @@ namespace FASTFOOD_DNC
                 if (txtHovaTen.Text == "" || txtDiachi.Text == "" || txtMatkhau.Text == "" || txtTentaikhoan.Text == "")
                 {
                     MessageBox.Show("Mời Bạn Nhập Đầy Đủ Thông Tin!","Thông Báo!",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                    return;
                 }
                 try
                 {
@@ -41,7 +42,29 @@ namespace FASTFOOD_DNC
                     {
                         MessageBox.Show("Kết Nối Cơ Sở Dữ Liệu Thành Công!","Kết Nối Dữ Liệu");
                     }
-                    string insertTaiKhoan = "INSERT INTO TAIKHOAN VALUES ('')";
+                    string insertTaiKhoan = @"INSERT INTO TAIKHOAN(TENDN,MK,VAITRO) VALUES(@TenDN,@MatKhau,N'Khach Hang');
+                                              SELECT CAST(SCOPE_IDENTITY() AS INT);";// Chọn ID vừa mới chèn   vào bản khách hàng
+                    using(SqlCommand cmdTaiKhoan = new SqlCommand(insertTaiKhoan, conn))
+                    {
+                        cmdTaiKhoan.Parameters.AddWithValue("@TenDN", txtTentaikhoan.Text);
+                        cmdTaiKhoan.Parameters.AddWithValue("@MatKhau", txtMatkhau.Text);
+                        int newTaiKhoanID = (int)cmdTaiKhoan.ExecuteScalar(); // Lấy ID vừa mới chèn
+
+                        string insertKhachHang = @"INSERT INTO KHACHHANG(HOTEN,DIACHI,SDT,TAIKHOAN_ID) 
+                                                   VALUES(@HoVaTen,@DiaChi,@SDT,@TaiKhoanID)";
+                        using (SqlCommand cmdKhachHang = new SqlCommand(insertKhachHang, conn))
+                        {
+                            cmdKhachHang.Parameters.AddWithValue("@HoVaTen", txtHovaTen.Text);
+                            cmdKhachHang.Parameters.AddWithValue("@DiaChi", txtDiachi.Text);
+                            cmdKhachHang.Parameters.AddWithValue("@SDT", txtSoDT.Text);
+                            cmdKhachHang.Parameters.AddWithValue("@TaiKhoanID", newTaiKhoanID);
+                            cmdKhachHang.ExecuteNonQuery();
+                        }
+                        MessageBox.Show("Đăng Ký Thành Công!", "Thông Báo!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        // chuyển sang form đăng nhập
+                        Form DangNhap = new frmDangNhap();
+                        this.Hide();
+                    }
                 }
                 catch (Exception ex) { 
                     
