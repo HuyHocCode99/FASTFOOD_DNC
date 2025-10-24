36d03079f1fffff1f7b9d6599a38bb89d679d088
khoa560560 with Dang Khoa <khoa560560@gmail.com>
2025-10-24 21:24:20 +0700

    đang làm giao diện menu khách hàng

--- FASTFOOD_DNC/ucDonHangKH.Designer.cs
+++ FASTFOOD_DNC/ucDonHangKH.Designer.cs
@@ -29,13 +29,17 @@
         private void InitializeComponent()
         {
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.label6 = new System.Windows.Forms.Label();
             this.lblTrangThai = new System.Windows.Forms.Label();
             this.lblTongTien = new System.Windows.Forms.Label();
             this.lblNgayDat = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtMaDon = new System.Windows.Forms.TextBox();
+            this.txtNgayDat = new System.Windows.Forms.TextBox();
+            this.txtTongTien = new System.Windows.Forms.TextBox();
+            this.txtTrangThai = new System.Windows.Forms.TextBox();
+            this.btnok = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -51,7 +55,11 @@
             // 
             // splitContainer1.Panel1
             // 
-            this.splitContainer1.Panel1.Controls.Add(this.label6);
+            this.splitContainer1.Panel1.Controls.Add(this.btnok);
+            this.splitContainer1.Panel1.Controls.Add(this.txtTrangThai);
+            this.splitContainer1.Panel1.Controls.Add(this.txtTongTien);
+            this.splitContainer1.Panel1.Controls.Add(this.txtNgayDat);
+            this.splitContainer1.Panel1.Controls.Add(this.txtMaDon);
             this.splitContainer1.Panel1.Controls.Add(this.lblTrangThai);
             this.splitContainer1.Panel1.Controls.Add(this.lblTongTien);
             this.splitContainer1.Panel1.Controls.Add(this.lblNgayDat);
@@ -61,24 +69,15 @@
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
-            this.splitContainer1.Size = new System.Drawing.Size(1093, 713);
-            this.splitContainer1.SplitterDistance = 523;
+            this.splitContainer1.Size = new System.Drawing.Size(989, 611);
+            this.splitContainer1.SplitterDistance = 473;
             this.splitContainer1.TabIndex = 0;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(228, 150);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(53, 20);
-            this.label6.TabIndex = 5;
-            this.label6.Text = "label6";
             // 
             // lblTrangThai
             // 
             this.lblTrangThai.AutoSize = true;
             this.lblTrangThai.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTrangThai.Location = new System.Drawing.Point(63, 283);
+            this.lblTrangThai.Location = new System.Drawing.Point(9, 390);
             this.lblTrangThai.Name = "lblTrangThai";
             this.lblTrangThai.Size = new System.Drawing.Size(117, 25);
             this.lblTrangThai.TabIndex = 4;
@@ -88,7 +87,7 @@
             // 
             this.lblTongTien.AutoSize = true;
             this.lblTongTien.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTongTien.Location = new System.Drawing.Point(63, 234);
+            this.lblTongTien.Location = new System.Drawing.Point(9, 309);
             this.lblTongTien.Name = "lblTongTien";
             this.lblTongTien.Size = new System.Drawing.Size(110, 25);
             this.lblTongTien.TabIndex = 3;
@@ -98,7 +97,7 @@
             // 
             this.lblNgayDat.AutoSize = true;
             this.lblNgayDat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNgayDat.Location = new System.Drawing.Point(63, 191);
+            this.lblNgayDat.Location = new System.Drawing.Point(9, 222);
             this.lblNgayDat.Name = "lblNgayDat";
             this.lblNgayDat.Size = new System.Drawing.Size(105, 25);
             this.lblNgayDat.TabIndex = 2;
@@ -108,7 +107,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(63, 142);
+            this.label2.Location = new System.Drawing.Point(9, 146);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(145, 25);
             this.label2.TabIndex = 1;
@@ -120,7 +119,7 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(523, 104);
+            this.label1.Size = new System.Drawing.Size(473, 104);
             this.label1.TabIndex = 0;
             this.label1.Text = "ĐƠN HÀNG CỦA BẠN";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -128,12 +127,56 @@
             // pictureBox1
             // 
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::FASTFOOD_DNC.Properties.Resources.fast_food_burger_king_2_696x597;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(566, 713);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.Size = new System.Drawing.Size(512, 611);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // txtMaDon
+            // 
+            this.txtMaDon.Location = new System.Drawing.Point(160, 144);
+            this.txtMaDon.Name = "txtMaDon";
+            this.txtMaDon.ReadOnly = true;
+            this.txtMaDon.Size = new System.Drawing.Size(310, 27);
+            this.txtMaDon.TabIndex = 5;
+            // 
+            // txtNgayDat
+            // 
+            this.txtNgayDat.Location = new System.Drawing.Point(160, 220);
+            this.txtNgayDat.Name = "txtNgayDat";
+            this.txtNgayDat.ReadOnly = true;
+            this.txtNgayDat.Size = new System.Drawing.Size(310, 27);
+            this.txtNgayDat.TabIndex = 6;
+            // 
+            // txtTongTien
+            // 
+            this.txtTongTien.Location = new System.Drawing.Point(160, 307);
+            this.txtTongTien.Name = "txtTongTien";
+            this.txtTongTien.ReadOnly = true;
+            this.txtTongTien.Size = new System.Drawing.Size(310, 27);
+            this.txtTongTien.TabIndex = 7;
+            // 
+            // txtTrangThai
+            // 
+            this.txtTrangThai.Location = new System.Drawing.Point(161, 388);
+            this.txtTrangThai.Name = "txtTrangThai";
+            this.txtTrangThai.ReadOnly = true;
+            this.txtTrangThai.Size = new System.Drawing.Size(310, 27);
+            this.txtTrangThai.TabIndex = 8;
+            // 
+            // btnok
+            // 
+            this.btnok.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnok.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnok.Location = new System.Drawing.Point(0, 526);
+            this.btnok.Name = "btnok";
+            this.btnok.Size = new System.Drawing.Size(473, 85);
+            this.btnok.TabIndex = 9;
+            this.btnok.Text = "OK";
+            this.btnok.UseVisualStyleBackColor = true;
             // 
             // ucDonHangKH
             // 
@@ -143,7 +186,7 @@
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "ucDonHangKH";
-            this.Size = new System.Drawing.Size(1093, 713);
+            this.Size = new System.Drawing.Size(989, 611);
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel1.PerformLayout();
             this.splitContainer1.Panel2.ResumeLayout(false);
@@ -162,7 +205,11 @@
         private System.Windows.Forms.Label lblTrangThai;
         private System.Windows.Forms.Label lblTongTien;
         private System.Windows.Forms.Label lblNgayDat;
-        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTrangThai;
+        private System.Windows.Forms.TextBox txtTongTien;
+        private System.Windows.Forms.TextBox txtNgayDat;
+        private System.Windows.Forms.TextBox txtMaDon;
+        private System.Windows.Forms.Button btnok;
     }
 }
--- FASTFOOD_DNC/ucMenuKH.Designer.cs
+++ FASTFOOD_DNC/ucMenuKH.Designer.cs
@@ -28,20 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.lblThemVaoGioHang = new System.Windows.Forms.Label();
+            this.lblDonGia = new System.Windows.Forms.Label();
+            this.lblTenMon = new System.Windows.Forms.Label();
+            this.picMonAn = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picMonAn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.textBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.lblThemVaoGioHang);
+            this.splitContainer1.Panel1.Controls.Add(this.lblDonGia);
+            this.splitContainer1.Panel1.Controls.Add(this.lblTenMon);
+            this.splitContainer1.Panel1.Controls.Add(this.picMonAn);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(842, 521);
+            this.splitContainer1.SplitterDistance = 394;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // lblThemVaoGioHang
+            // 
+            this.lblThemVaoGioHang.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblThemVaoGioHang.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThemVaoGioHang.Location = new System.Drawing.Point(0, 436);
+            this.lblThemVaoGioHang.Name = "lblThemVaoGioHang";
+            this.lblThemVaoGioHang.Size = new System.Drawing.Size(394, 85);
+            this.lblThemVaoGioHang.TabIndex = 3;
+            this.lblThemVaoGioHang.Text = "Thêm vào giỏ hàng:";
+            this.lblThemVaoGioHang.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDonGia
+            // 
+            this.lblDonGia.AutoSize = true;
+            this.lblDonGia.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDonGia.Location = new System.Drawing.Point(3, 292);
+            this.lblDonGia.Name = "lblDonGia";
+            this.lblDonGia.Size = new System.Drawing.Size(85, 22);
+            this.lblDonGia.TabIndex = 2;
+            this.lblDonGia.Text = "Đơn giá:";
+            this.lblDonGia.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTenMon
+            // 
+            this.lblTenMon.AutoSize = true;
+            this.lblTenMon.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTenMon.Location = new System.Drawing.Point(3, 192);
+            this.lblTenMon.Name = "lblTenMon";
+            this.lblTenMon.Size = new System.Drawing.Size(94, 22);
+            this.lblTenMon.TabIndex = 1;
+            this.lblTenMon.Text = "Tên món:";
+            this.lblTenMon.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // picMonAn
+            // 
+            this.picMonAn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.picMonAn.Location = new System.Drawing.Point(0, 0);
+            this.picMonAn.Name = "picMonAn";
+            this.picMonAn.Size = new System.Drawing.Size(394, 130);
+            this.picMonAn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picMonAn.TabIndex = 0;
+            this.picMonAn.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(444, 521);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(104, 191);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 27);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(104, 287);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(260, 27);
+            this.textBox2.TabIndex = 5;
             // 
             // ucMenuKH
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.splitContainer1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "ucMenuKH";
-            this.Size = new System.Drawing.Size(1072, 716);
+            this.Size = new System.Drawing.Size(842, 521);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picMonAn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblThemVaoGioHang;
+        private System.Windows.Forms.Label lblDonGia;
+        private System.Windows.Forms.Label lblTenMon;
+        private System.Windows.Forms.PictureBox picMonAn;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
