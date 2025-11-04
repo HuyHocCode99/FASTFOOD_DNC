54b09f3be79ecec5375448d94708a0ea856dac24
khoa560560 with Dang Khoa <khoa560560@gmail.com>
2025-11-04 09:36:57 +0700

    sửa ảnh giao diện

--- FASTFOOD_DNC/Admin.Designer.cs
+++ FASTFOOD_DNC/Admin.Designer.cs
@@ -35,18 +35,18 @@
             this.btnDonHang = new System.Windows.Forms.Button();
             this.lblUserName = new System.Windows.Forms.Label();
             this.lblWelcome = new System.Windows.Forms.Label();
-            this.picLogo = new System.Windows.Forms.PictureBox();
             this.pnlHeader = new System.Windows.Forms.Panel();
-            this.picUserIcon = new System.Windows.Forms.PictureBox();
             this.lblCurrentUserInfo = new System.Windows.Forms.Label();
             this.lnkSignOut = new System.Windows.Forms.LinkLabel();
             this.lblPageTitle = new System.Windows.Forms.Label();
             this.pnlMainContent = new System.Windows.Forms.Panel();
             this.pnMenu = new System.Windows.Forms.Panel();
+            this.picUserIcon = new System.Windows.Forms.PictureBox();
+            this.picLogo = new System.Windows.Forms.PictureBox();
             this.pnlNavigation.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
             this.pnlHeader.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picUserIcon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
             this.SuspendLayout();
             // 
             // pnlNavigation
@@ -90,7 +90,7 @@
             this.btnKhachHang.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnKhachHang.ForeColor = System.Drawing.Color.White;
             this.btnKhachHang.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnKhachHang.Location = new System.Drawing.Point(0, 412);
+            this.btnKhachHang.Location = new System.Drawing.Point(0, 441);
             this.btnKhachHang.Name = "btnKhachHang";
             this.btnKhachHang.Size = new System.Drawing.Size(230, 100);
             this.btnKhachHang.TabIndex = 6;
@@ -107,7 +107,7 @@
             this.btnMenu.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnMenu.ForeColor = System.Drawing.Color.White;
             this.btnMenu.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnMenu.Location = new System.Drawing.Point(0, 312);
+            this.btnMenu.Location = new System.Drawing.Point(0, 341);
             this.btnMenu.Name = "btnMenu";
             this.btnMenu.Size = new System.Drawing.Size(230, 100);
             this.btnMenu.TabIndex = 5;
@@ -124,7 +124,7 @@
             this.btnDonHang.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDonHang.ForeColor = System.Drawing.Color.White;
             this.btnDonHang.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnDonHang.Location = new System.Drawing.Point(0, 212);
+            this.btnDonHang.Location = new System.Drawing.Point(0, 241);
             this.btnDonHang.Name = "btnDonHang";
             this.btnDonHang.Size = new System.Drawing.Size(230, 100);
             this.btnDonHang.TabIndex = 4;
@@ -138,7 +138,7 @@
             this.lblUserName.Dock = System.Windows.Forms.DockStyle.Top;
             this.lblUserName.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblUserName.ForeColor = System.Drawing.Color.White;
-            this.lblUserName.Location = new System.Drawing.Point(0, 190);
+            this.lblUserName.Location = new System.Drawing.Point(0, 219);
             this.lblUserName.Name = "lblUserName";
             this.lblUserName.Size = new System.Drawing.Size(63, 22);
             this.lblUserName.TabIndex = 2;
@@ -151,24 +151,13 @@
             this.lblWelcome.Dock = System.Windows.Forms.DockStyle.Top;
             this.lblWelcome.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblWelcome.ForeColor = System.Drawing.Color.White;
-            this.lblWelcome.Location = new System.Drawing.Point(0, 158);
+            this.lblWelcome.Location = new System.Drawing.Point(0, 187);
             this.lblWelcome.Name = "lblWelcome";
             this.lblWelcome.Padding = new System.Windows.Forms.Padding(0, 10, 0, 0);
             this.lblWelcome.Size = new System.Drawing.Size(86, 32);
             this.lblWelcome.TabIndex = 1;
             this.lblWelcome.Text = "Xin chào,";
             this.lblWelcome.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // picLogo
-            // 
-            this.picLogo.Dock = System.Windows.Forms.DockStyle.Top;
-            this.picLogo.Image = global::FASTFOOD_DNC.Properties.Resources.Gemini_Generated_Image_5t53ly5t53ly5t53;
-            this.picLogo.Location = new System.Drawing.Point(0, 0);
-            this.picLogo.Name = "picLogo";
-            this.picLogo.Size = new System.Drawing.Size(230, 158);
-            this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.picLogo.TabIndex = 0;
-            this.picLogo.TabStop = false;
             // 
             // pnlHeader
             // 
@@ -182,17 +171,6 @@
             this.pnlHeader.Name = "pnlHeader";
             this.pnlHeader.Size = new System.Drawing.Size(1288, 50);
             this.pnlHeader.TabIndex = 1;
-            // 
-            // picUserIcon
-            // 
-            this.picUserIcon.Dock = System.Windows.Forms.DockStyle.Right;
-            this.picUserIcon.Image = global::FASTFOOD_DNC.Properties.Resources.teacher_240_1128987;
-            this.picUserIcon.Location = new System.Drawing.Point(1096, 0);
-            this.picUserIcon.Name = "picUserIcon";
-            this.picUserIcon.Size = new System.Drawing.Size(67, 50);
-            this.picUserIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.picUserIcon.TabIndex = 3;
-            this.picUserIcon.TabStop = false;
             // 
             // lblCurrentUserInfo
             // 
@@ -251,6 +229,28 @@
             this.pnMenu.Size = new System.Drawing.Size(1288, 789);
             this.pnMenu.TabIndex = 3;
             // 
+            // picUserIcon
+            // 
+            this.picUserIcon.Dock = System.Windows.Forms.DockStyle.Right;
+            this.picUserIcon.Image = global::FASTFOOD_DNC.Properties.Resources.teacher_240_1128987;
+            this.picUserIcon.Location = new System.Drawing.Point(1096, 0);
+            this.picUserIcon.Name = "picUserIcon";
+            this.picUserIcon.Size = new System.Drawing.Size(67, 50);
+            this.picUserIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picUserIcon.TabIndex = 3;
+            this.picUserIcon.TabStop = false;
+            // 
+            // picLogo
+            // 
+            this.picLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.picLogo.Image = global::FASTFOOD_DNC.Properties.Resources._53400069_cáo_bảng_đen_fastfood___hamburger__khoai_tây_chiên_và_hotdog__vector_hình_minh_họa_;
+            this.picLogo.Location = new System.Drawing.Point(0, 0);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(230, 187);
+            this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picLogo.TabIndex = 0;
+            this.picLogo.TabStop = false;
+            // 
             // frmAdmin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
@@ -269,10 +269,10 @@
             this.Load += new System.EventHandler(this.frmAdmin_Load);
             this.pnlNavigation.ResumeLayout(false);
             this.pnlNavigation.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
             this.pnlHeader.ResumeLayout(false);
             this.pnlHeader.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picUserIcon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
